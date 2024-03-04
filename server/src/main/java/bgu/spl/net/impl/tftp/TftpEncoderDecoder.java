0b351f5c6eb67c43388888760fdde42e8cda1a79
FoyerD <amitfoye@post.bgu.ac.il>
2024-03-04 22:58:56 +0200

    decode now gets optcode

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
@@ -1,15 +1,32 @@
 package bgu.spl.net.impl.tftp;
 
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+
 import bgu.spl.net.api.MessageEncoderDecoder;
 
 public class TftpEncoderDecoder implements MessageEncoderDecoder<byte[]> {
-    //TODO: Implement here the TFTP encoder and decoder
+    private final ByteBuffer optcodeBuffer = ByteBuffer.allocate(2);
+    private short optcode = -1;
+    private int objectBytesIndex = 0;
 
 
     @Override
     public byte[] decodeNextByte(byte nextByte) {
-        // TODO: implement this
+        //TODO implement
 
+        if (optcode == -1) { //indicates that we are still reading the optcode
+            optcodeBuffer.put(nextByte);
+            if (!optcodeBuffer.hasRemaining()) { //we read 2 bytes and therefore can take the length
+                optcodeBuffer.flip();
+                optcode = optcodeBuffer.getShort();
+                objectBytesIndex = 0;
+                optcodeBuffer.clear();
+            }
+        } else {
+            optcode = -1;
+            return null;
+        }
         return null;
     }
 
