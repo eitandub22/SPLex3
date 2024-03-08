d6eee11b3d886784cda4b84c5f3729b5dcdfba37
eitandub22 <eitandu@post.bgu.ac.il>
2024-03-09 00:02:58 +0200

    started tftpServer

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
@@ -1,10 +1,9 @@
 package bgu.spl.net.impl.tftp;
 
+import bgu.spl.net.api.BidiMessagingProtocol;
 import bgu.spl.net.api.MessageEncoderDecoder;
 import bgu.spl.net.api.MessagingProtocol;
-import bgu.spl.net.srv.BaseServer;
-import bgu.spl.net.srv.BlockingConnectionHandler;
-import bgu.spl.net.srv.Server;
+import bgu.spl.net.srv.*;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -16,24 +15,22 @@ import java.util.function.Supplier;
 public class TftpServer implements Server<byte[]>{
     private ConcurrentHashMap<String, Integer> loggedUsers;
     private final int port;
-    private final Supplier<MessagingProtocol<T>> protocolFactory;
-    private final Supplier<MessageEncoderDecoder<T>> encdecFactory;
+    private final Supplier<BidiMessagingProtocol<byte[]>> protocolFactory;
+    private final Supplier<MessageEncoderDecoder<byte[]>> encdecFactory;
     private ServerSocket sock;
-
-    public BaseServer(
-            int port,
-            Supplier<MessagingProtocol<T>> protocolFactory,
-            Supplier<MessageEncoderDecoder<T>> encdecFactory) {
-
+    private final Connections<byte[]> connections;
+    private int connectionId = 0;
+    public TftpServer(int port, Supplier<BidiMessagingProtocol<byte[]>> protocolFactory, Supplier<MessageEncoderDecoder<byte[]>> encdecFactory) {
         this.port = port;
         this.protocolFactory = protocolFactory;
         this.encdecFactory = encdecFactory;
-        this.sock = null;
+        this.connections = new ConnectionsImpl();
     }
-
+    protected void execute(BlockingConnectionHandler<byte[]> handler) {
+        new Thread(handler).run();
+    }
     @Override
     public void serve() {
-
         try (ServerSocket serverSock = new ServerSocket(port)) {
             System.out.println("Server started");
 
@@ -43,15 +40,15 @@ public class TftpServer implements Server<byte[]>{
 
                 Socket clientSock = serverSock.accept();
 
-                BlockingConnectionHandler<T> handler = new BlockingConnectionHandler<>(
+                BlockingConnectionHandler<byte[]> handler = new BlockingConnectionHandler<>(
                         clientSock,
                         encdecFactory.get(),
                         protocolFactory.get());
-
+                connections.connect(this.connectionId, handler);
+                this.connectionId++;
                 execute(handler);
             }
-        }
-        catch (IOException ex) {
+        } catch (IOException ex) {
         }
 
         System.out.println("server closed!!!");
@@ -62,9 +59,5 @@ public class TftpServer implements Server<byte[]>{
         if (sock != null)
             sock.close();
     }
-    protected void execute(BlockingConnectionHandler<byte[]> handler) {
-        new Thread(handler).run();
-    }
-
 }
 
