fbb2fe9e8f3a83aa8f6f909248ba51dcd12000e3
FoyerD <amitfoye@post.bgu.ac.il>
2024-03-10 13:24:07 +0200

    startd imlementing

--- server/src/main/java/bgu/spl/net/impl/tftp/packetReaders/DATAreader.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/packetReaders/DATAreader.java
@@ -1,5 +1,23 @@
 package bgu.spl.net.impl.tftp.packetReaders;
 
-public class DATAreader {
+import java.nio.ByteBuffer;
 
+public class DATAreader extends PacketReader{
+    public static final short OPTCODE = 3;
+
+    public DATAreader(){
+        this.pBuffer = ByteBuffer.wrap(new byte[]{0,OPTCODE}, 2, 518);
+    }
+
+    public byte[] proccesByte(byte b){
+        if(b == 0){
+            byte[] retArr = this.pBuffer.array().clone();
+            this.pBuffer.flip();
+            this.pBuffer.clear();
+            return retArr;
+        }
+
+        this.pBuffer.put(b);
+        return null;
+    }
 }
