d7cd3b1a6dd5cf41c8ef8698febc6750ad33901e
eitandub22 <eitandu@post.bgu.ac.il>
2024-03-11 16:20:57 +0200

    deleted decodeNextByte code, don't know why is had incomplete code

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
@@ -13,41 +13,10 @@ public class TftpEncoderDecoder implements MessageEncoderDecoder<byte[]> {
     private int packetBytesIndex = 0;
     private byte[] returnMsg = null;
 
-
     @Override
     public byte[] decodeNextByte(byte nextByte) {
         //TODO implement
-
-        if (optcode == -1) { //indicates that we are still reading the optcode
-            optcodeBuffer.put(nextByte);
-            if (!optcodeBuffer.hasRemaining()) { //we read 2 bytes and therefore can take the length
-                optcodeBuffer.flip();
-                optcode = optcodeBuffer.getShort();
-                optcodeBuffer.clear();
-            }
-        } else {
-            switch (optcode) {
-                case 1:
-                    
-                    break;
-                case 2:
-                    
-                    break;
-                case 3:
-                    
-                    break;
-                case 6:
-                    return 
-                    break;
-            
-                default:
-                    break;
-            }
-
-            optcode = -1;
-            return null;
-        }
-        return null;
+        return new byte[0];
     }
 
     @Override
