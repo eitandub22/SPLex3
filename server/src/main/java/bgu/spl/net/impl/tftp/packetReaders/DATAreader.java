1d855c899920959c219a9ddfccb899480d121b6b
FoyerD <amitfoye@post.bgu.ac.il>
2024-03-11 22:00:46 +0200

    finished DATAreader

--- server/src/main/java/bgu/spl/net/impl/tftp/packetReaders/DATAreader.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/packetReaders/DATAreader.java
@@ -4,22 +4,42 @@ import java.nio.ByteBuffer;
 
 public class DATAreader extends PacketReader{
     public static final short OPTCODE = 3;
-    private static shortBuffer;
+    private short size;
+    private short pNum;
+    private ByteBuffer shortBuffer;
 
     public DATAreader(){
         this.pBuffer = ByteBuffer.wrap(new byte[]{0,OPTCODE}, 2, 518);
-        private static shortBuffer
+        this.shortBuffer = ByteBuffer.allocate(2);
+        this.pNum = -1;
+        this.size = -1;
     }
 
     public byte[] proccesByte(byte b){
-        if(b == 0){
-            byte[] retArr = this.pBuffer.array().clone();
+        this.pBuffer.put(b);
+        if(this.size == -1){
+            this.shortBuffer.put(b);
+            if(!this.shortBuffer.hasRemaining()){
+                this.shortBuffer.flip();
+                this.size = this.shortBuffer.getShort();
+                this.shortBuffer.clear();
+            }
+        }
+        else if(this.pNum == -1){
+            this.shortBuffer.put(b);
+            if(!this.shortBuffer.hasRemaining()){
+                this.shortBuffer.flip();
+                this.pNum = this.shortBuffer.getShort();
+                this.shortBuffer.clear();
+            }
+        }
+        else if(this.pBuffer.position() == this.size + 6){
             this.pBuffer.flip();
-            this.pBuffer.clear();
+            byte[] retArr = new byte[this.pBuffer.remaining()];
+            this.pBuffer.get(retArr);
             return retArr;
         }
 
-        this.pBuffer.put(b);
         return null;
     }
 }
